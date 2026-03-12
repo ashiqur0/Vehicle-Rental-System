3608ef66eb0147614b26e2da1494ac865ec95f6a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-03-12 19:35:45 +0600

    Add vehicle creation functionality with routes and services

---
+++ src/modules/vehicles/vehicle.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express"
+import { vehicleServices } from "./vehicle.service";
+
+const createVehicle = async (req: Request, res: Response) => {
+    try {
+        const result = await vehicleServices.createVehicle(req.body);
+        
+        res.status(201).json({
+            success: true,
+            message: "Vehicle created successfull",
+            data: result.rows[0]
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            details: error
+        })
+    }
+}
+
+export const vehicleController = {
+    createVehicle,
+}
