b7583a8ce97cbba10c57a21ae3c6025f645804f2
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-03-13 12:29:34 +0600

    fix: update signup service to include user role and adjust token expiration in signin

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -3,6 +3,7 @@ import { authController } from './auth.controller';
 
 const router = express.Router();
 
+// public routes
 router.post('/signup', authController.signup);
 router.post('/signin', authController.signin);
 
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,14 +4,14 @@ import jwt from 'jsonwebtoken';
 import config from "../../config";
 
 const signup = async (payload: Record<string, string>) => {
-    const { name, email, password, phone } = payload;
+    const { name, email, password, phone, role = "customer" } = payload;
 
     if (password!.length < 6 || !email || !name || !phone) return null;
     if (email !== email.toLowerCase()) return null;
     const hashedPassword = await bcrypt.hash(password as string, 10);
 
     const result = await pool.query(`
-        INSERT INTO users (name, email, password, phone) VALUES ($1, $2, $3, $4) RETURNING *`, [name, email, hashedPassword, phone]
+        INSERT INTO users (name, email, password, phone, role) VALUES ($1, $2, $3, $4, $5) RETURNING *`, [name, email, hashedPassword, phone, role]
     );
 
     return result;
@@ -24,10 +24,9 @@ const signin = async (email: string, password: string) => {
 
     const user = result.rows[0];
     const match = await bcrypt.compare(password, user.password);
-    console.log({ match, password, password2: user.password, email, email2: user.email })
     if (!match) return false;
 
-    const token = jwt.sign({ name: user.name, email: user.email, role: user.role }, config.jwt_secret as string, { expiresIn: '7d' });
+    const token = jwt.sign({ name: user.name, email: user.email, role: user.role }, config.jwt_secret as string, { expiresIn: '3h' });
 
     return { token };
 }
