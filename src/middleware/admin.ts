e0243ace9fe85861c0a4fb0601f4135176934bce
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-03-13 14:30:14 +0600

    feat: add admin verification middleware to enforce role-based access control

---
+++ src/middleware/admin.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+
+const verifyAdmin = (role: string) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            if (role !== req?.user?.role) {
+                return res.status(403).json({
+                    success: false,
+                    message: "Forbidden"
+                });
+            }
+
+            next();
+        } catch (error: any) {
+            res.status(500).json({
+                success: false,
+                message: error.message,
+                details: error.stack
+            })
+        }
+    }
+};
+
+export default verifyAdmin;
