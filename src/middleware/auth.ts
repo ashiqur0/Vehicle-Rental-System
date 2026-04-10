2905479693f82b832b77922617c9b4c2df5d5ad1
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-04-10 13:00:09 +0600

    refactor: streamline auth middleware error handling and improve token validation logic

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,37 +1,33 @@
 import { NextFunction, Request, Response } from "express";
-import config from "../config";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import config from "../config";
 
 const auth = (...roles: string[]) => {
     return async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const token = req.headers.authorization;
-            if (!token) {
-                return res.status(401).json({
-                    success: false,
-                    message: "Unauthorized"
-                })
-            }
+        let token = req.headers.authorization;
 
-            const decoded = jwt.verify(token, config.jwt_secret as string) as JwtPayload;
-            req.user = decoded;
+        if (token && token.startsWith('Bearer')) {
+            try {
+                token = token.split(' ')[1];
 
-            if (roles.length && !roles.includes(decoded.role)) {
-                return res.status(403).json({
-                    success: false,
-                    message: "Forbidden"
-                });
+                const decoded = jwt.verify(token!, config.jwt_secret as string) as JwtPayload;
+                req.user = decoded;
+
+                if (roles.length && !roles.includes(decoded.role)) {
+                    return res.status(403).json({
+                        success: false,
+                        message: "Forbidden"
+                    });
+                }
+
+                next();
+            } catch (error) {
+                res.status(401).json({ success: false, message: 'Not authorized, token failed' });
             }
-            
-            next();
-        } catch (error: any) {
-            res.status(500).json({
-                success: false,
-                message: error.message,
-                details: error.stack
-            })
+        } else {
+            res.status(401).json({ success: false, message: 'No token, authorization denied' });
         }
     }
-};
+}
 
 export default auth;
--- src/modules/vehicles/vehicle.route.ts
+++ src/modules/vehicles/vehicle.route.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { vehicleController } from './vehicle.controller';
-import auth from '../../middleware/auth';
 import verifyAdmin from '../../middleware/admin';
+import auth from '../../middleware/auth';
 
 const router = express.Router();
 
 // admin only routes
 router.post('/', auth('admin'), verifyAdmin, vehicleController.createVehicle);
+
 router.put('/:vehicleId', auth('admin'), verifyAdmin, vehicleController.updateVehicle);
 router.delete('/:vehicleId', auth('admin'), verifyAdmin, vehicleController.deleteVehicle);
 
